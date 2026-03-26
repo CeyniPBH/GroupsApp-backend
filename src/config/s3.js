6736f12531591ed096591c1c1d1c54595aef9032
johan <jemesav@eafit.edu.co>
2026-03-26 15:28:02 -0500

    small fixes to .env and .gitignore, and added s3

---
+++ src/config/s3.js
@@ -0,0 +1,30 @@
+const { S3Client } = require('@aws-sdk/client-s3');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+require('dotenv').config();
+
+// Configuramos el cliente de AWS
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+});
+
+// Creamos el middleware de Multer pero usando S3 como almacenamiento
+const uploadToS3 = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_S3_BUCKET_NAME,
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            cb(null, 'uploads/' + uniqueSuffix + '-' + file.originalname);
+        }
+    })
+});
+
+module.exports = { s3, uploadToS3 };
