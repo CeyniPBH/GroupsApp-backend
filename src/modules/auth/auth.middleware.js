5eb76da2422eb35884c1ba3bc1b53552874b422a
johan <jemesav@eafit.edu.co>
2026-03-28 23:18:04 -0500

    env fix

--- src/modules/auth/auth.middleware.js
+++ src/modules/auth/auth.middleware.js
@@ -10,7 +10,7 @@ const verifyToken = (req, res, next) => {
     const token = authHeader.split(' ')[1]; // Assuming "Bearer <token>"
 
     try {
-        const decoded = jwt.verify(token, 'secret123');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
