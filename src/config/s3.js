1173ec8e7909624ac10dffcea4f63c203feff64f
johan <jemesav@eafit.edu.co>
2026-03-28 20:00:24 -0500

    EC2 ajustes for front

--- src/config/s3.js
+++ src/config/s3.js
@@ -5,11 +5,7 @@ require('dotenv').config();
 
 // Configuramos el cliente de AWS
 const s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    }
+    region: process.env.AWS_REGION
 });
 
 // Creamos el middleware de Multer pero usando S3 como almacenamiento
